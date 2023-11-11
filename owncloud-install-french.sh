5df0543ca6d518b827556dd9d4ad1df3a8bdc0f9
NKanzallah <119493977+TheGostOfNight@users.noreply.github.com>
2023-11-11 23:57:17 +0100

    Add files via upload

---
+++ owncloud-install-french.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Ce fichier a été créé par TheGoasOfNight
+
+# Exécutez ce fichier avec l'utilisateur root
+
+# Avant d'exécuter le script, assurez-vous de disposer de :
+# Connexion Internet
+# Debian 12.x.x ou version ultérieure
+
+# ------------------------------------------------
+
+# Assurez-vous que le cache des paquets système est à jour
+apt update
+
+# Installez le serveur web Apache
+apt install apache2
+
+# Assurez-vous que le service Apache2 est démarré
+systemctl enable --now apache2
+
+# Assurez-vous que le cache des paquets système est à jour
+apt update
+
+# Installez MariaDB 10
+apt install mariadb-server
+
+# Assurez-vous que MariaDB est en cours d'exécution
+systemctl enable --now mariadb
+
+# Installez le dépôt SURY APT
+apt update
+apt -y install apt-transport-https lsb-release ca-certificates curl wget gnupg2
+
+wget -qO- https://packages.sury.org/php/apt.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/sury-php.gpg
+
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+
+# Assurez-vous que le cache des paquets système est à jour
+apt update
+
+# Installez PHP 7.x
+apt install php7.4
+
+# Installez les modules PHP supplémentaires
+apt install libapache2-mod-php7.4 php7.4-{mysql,intl,curl,gd,xml,mbstring,zip} -y
+
+# Installez le dépôt ownCloud
+echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/server:/10/Debian_12/ /' | tee /etc/apt/sources.list.d/isv:ownCloud:server:10.list
+curl -fsSL https://download.opensuse.org/repositories/isv:ownCloud:server:10/Debian_12/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/isv_ownCloud_server_10.gpg > /dev/null
+apt update
+apt install owncloud-complete-files -y
+
+# Configurez Apache pour ownCloud
+cat > /etc/apache2/sites-available/owncloud.conf << 'EOL'
+Alias / "/var/www/owncloud/"
+
+<Directory /var/www/owncloud/>
+  Options +FollowSymlinks
+  AllowOverride All
+
+  <IfModule mod_dav.c>
+    Dav off
+  </IfModule>
+
+  SetEnv HOME /var/www/owncloud
+  SetEnv HTTP_HOME /var/www/owncloud
+</Directory>
+EOL
+
+# Rechargez le fichier de configuration
+systemctl reload apache2
+
+# Activez le site OwnCloud
+a2ensite owncloud.conf
+
+# Désactivez le site Apache par défaut
+a2dissite 000-default.conf
+
+# Activez les modules Apache recommandés supplémentaires
+a2enmod rewrite mime unique_id php7.4
+
+# Mettez à jour la propriété du répertoire racine ownCloud
+chown -R www-data: /var/www/owncloud
+
+# Redémarrez Apache2
+systemctl restart apache2
+
+# Désactivez la connexion à distance en tant que root dans MariaDB
+mysql <<EOF
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+FLUSH PRIVILEGES;
+
+# Créez la base de données et l'utilisateur de la base de données pour ownCloud
+CREATE DATABASE db_owncloud;
+GRANT ALL ON db_owncloud.* TO 'ow_admin'@'localhost' IDENTIFIED BY 'xooL9YjS';
+FLUSH PRIVILEGES;
+EOF
